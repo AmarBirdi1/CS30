bc818e42f211f3c2731e7925ae8f6f12fd5eed81
904237013 <904237013@L815WD150059.edu.cbe.ab.ca>
2022-03-09 10:47:04 -0700

    Break

--- Chapters10/src/BreakAPlate.java
+++ Chapters10/src/BreakAPlate.java
@@ -5,7 +5,7 @@
 // Modify the BreakAPlate application to display a picture of the prize won rather than text naming the prize. 
 //The tiger_plush.gif, sticker.gif, and 
 // placeholder.gif are supplied as data files for this text. The placeholder.gif file should be displayed in the label and at the start 
-// of each game.
+// of each game
 
 
 import javax.swing.*;
